83ff4d3018a57a5b9c15fd9f4a677dad15cd8251
s0a1e2 <oops2198@gmail.com>
2023-12-21 16:04:34 +0330

    import USERS_SERVICE to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USERS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'users_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
