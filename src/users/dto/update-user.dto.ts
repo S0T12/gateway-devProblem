0a187976875fc915c70812886aed133e12082570
s0a1e2 <oops2198@gmail.com>
2023-12-25 18:39:42 +0330

    add users DTO's

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,12 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsNumber } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PickType(CreateUserDto, [
+  'name',
+  'password',
+  'email',
+]) {
+  @IsNumber()
+  id: number;
+}
