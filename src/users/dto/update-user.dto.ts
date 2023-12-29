e078aa56cf7be26c865f8d977459afcc0327eb4b
s0a1e2 <oops2198@gmail.com>
2023-12-29 15:38:38 +0330

    update update DTO

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,12 +1,8 @@
 import { PickType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNumber } from 'class-validator';
 
 export class UpdateUserDto extends PickType(CreateUserDto, [
   'name',
   'password',
   'email',
-]) {
-  @IsNumber()
-  id: number;
-}
+]) {}
