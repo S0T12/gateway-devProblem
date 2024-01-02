86b2a25d43845512b7e238d969a4fddfc3682408
s0a1e2 <oops2198@gmail.com>
2024-01-03 02:30:18 +0330

    config problems module

--- src/problems/problems.module.ts
+++ src/problems/problems.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ProblemsService } from './problems.service';
 import { ProblemsController } from './problems.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'PROBLEMS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'problems_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [ProblemsController],
   providers: [ProblemsService],
 })
