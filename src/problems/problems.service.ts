3ffce45cfa41fc76eb89b24840879e5fc8e5dafe
s0a1e2 <oops2198@gmail.com>
2024-01-03 02:38:26 +0330

    config problems service

--- src/problems/problems.service.ts
+++ src/problems/problems.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateProblemDto } from './dto/create-problem.dto';
 import { UpdateProblemDto } from './dto/update-problem.dto';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class ProblemsService {
+  constructor(
+    @Inject('PROBLEMS_SERVICE') private readonly client: ClientProxy,
+  ) {
+    this.client.connect();
+  }
   create(createProblemDto: CreateProblemDto) {
-    return 'This action adds a new problem';
+    return this.client.send('createProblem', createProblemDto);
   }
 
   findAll() {
-    return `This action returns all problems`;
+    const problems = this.client.send('findAllProblems', {});
+    return problems;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} problem`;
+    const problem = this.client.send('findOneProblem', id);
+    return problem;
   }
 
   update(id: number, updateProblemDto: UpdateProblemDto) {
-    return `This action updates a #${id} problem`;
+    const problem = this.client.send('updateProblem', {
+      id,
+      updateProblemDto,
+    });
+    return problem;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} problem`;
+    const problem = this.client.send('removeProblem', id);
+    return problem;
   }
 }
