01fae0337aa0b7e188ba97b194f8cfc4b6c2cf70
s0a1e2 <oops2198@gmail.com>
2024-01-05 18:29:02 +0330

    update connections

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,37 +1,85 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ClientProxy } from '@nestjs/microservices';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
+  private isConnected = false;
   constructor(@Inject('USERS_SERVICE') private client: ClientProxy) {
-    this.client.connect();
+    this.connectToClient();
+  }
+
+  private async connectToClient() {
+    try {
+      await this.client.connect();
+      this.isConnected = true;
+      this.logger.log('Connected to the client successfully');
+    } catch (error) {
+      this.logger.error(
+        `Error connecting to client: ${error.message}`,
+        error.stack,
+      );
+    }
   }
 
   async create(createUserDto: CreateUserDto): Promise<object> {
-    const result = this.client.send('createUser', createUserDto);
-    return result;
+    if (!this.isConnected) {
+      this.logger.warn('Client is not connected!');
+      return of();
+    }
+    return this.client.send('createUser', createUserDto);
   }
 
   async findAll(): Promise<object> {
-    const users = this.client.send('findAllUsers', {});
-    return users;
+    if (!this.isConnected) {
+      this.logger.warn('Client is not connected!');
+      return of();
+    }
+    return this.client.send('findAllUsers', {});
   }
 
   findOne(id: number): Observable<object> {
-    const user = this.client.send('findOneUser', id);
-    return user;
+    if (!this.isConnected) {
+      this.logger.warn('Client is not connected!');
+      return of();
+    }
+    return this.client.send('findOneUser', id);
   }
 
   update(id: number, updateUserDto: UpdateUserDto): Observable<object> {
-    const user = this.client.send('updateUser', { id, updateUserDto });
-    return user;
+    if (!this.isConnected) {
+      this.logger.warn('Client is not connected!');
+      return of();
+    }
+    return this.client.send('updateUser', { id, updateUserDto });
   }
 
   remove(id: number): Observable<object> {
-    const user = this.client.send('removeUser', id);
-    return user;
+    if (!this.isConnected) {
+      this.logger.warn('Client is not connected!');
+      return of();
+    }
+    return this.client.send('removeUser', id);
+  }
+
+  async onApplicationShutdown(signal?: string) {
+    this.logger.log(
+      `Application is shutting down (Signal: ${signal || 'undefined'})`,
+    );
+
+    await this.close();
+
+    this.logger.log('Application shutdown completed');
+  }
+
+  async close() {
+    if (this.isConnected) {
+      await this.client.close();
+      this.isConnected = false;
+      this.logger.log('Client connection closed gracefully');
+    }
   }
 }
